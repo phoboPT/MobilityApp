1c2d88c39c7afda1e20ad2f27789b436bd6f9777
phoboPT <hugo.branco.c@gmail.com>
2021-04-28 22:53:04 +0100

    Testes

---
+++ auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import cookieSession from 'cookie-session';
+
+const app = express()
+app.set('trust proxy', true)
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV!=='test',
+})
+)
+
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
+
+app.all('*', async () => {
+    throw new NotFoundError({ from: 'Index, /BAD_URL, route don\'t exist ' })
+})
+
+app.use(errorHandler)
+
+export { app }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,6 @@
-import express from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
 import mongoose from 'mongoose'
+import { app } from './app'
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-import cookieSession from 'cookie-session';
-
-const app = express()
-app.set('trust proxy', true)
-app.use(json());
-app.use(cookieSession({
-    signed: false,
-    secure: true,
-})
-)
-
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
-
-app.all('*', async () => {
-    throw new NotFoundError({ from: 'Index, /BAD_URL, route don\'t exist ' })
-})
-
-app.use(errorHandler)
 const start = async () => {
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY not defined')
