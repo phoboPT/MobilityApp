bd445904a445de0e4567effe4cdb5b79226bef18
phoboPT <hugo.branco.c@gmail.com>
2021-05-07 10:51:41 +0100

    events listeners

--- vehicles/src/routes/new.ts
+++ vehicles/src/routes/new.ts
@@ -1,7 +1,9 @@
 import { requiredAuth, validateRequest } from "@mobileorg/common-lib"
 import express, { Response, Request } from "express"
 import { body } from 'express-validator'
+import { VehiculeCreatedPublisher } from "../events/publishers/vehicule-created-publisher"
 import { Vehicle } from "../models/vehicle"
+import { natsWrapper } from "../nats-wrapper"
 
 const router = express.Router()
 
@@ -9,17 +11,23 @@ router.post('/api/vehicles', requiredAuth, [
     body('location').not().isEmpty().withMessage('location required'),
     body('type').not().isEmpty().withMessage('type required')
 ], validateRequest, async (req: Request, res: Response) => {
-
-
     const { location, type } = req.body
 
-    const vehicle = Vehicle.build({
+    const vehicule = Vehicle.build({
         location,
         type,
         userId: req.currentUser!.id
     })
-    await vehicle.save()
-    res.status(201).send(vehicle)
+    await vehicule.save()
+
+    new VehiculeCreatedPublisher(natsWrapper.client).publish({
+        id: vehicule.id,
+        type: vehicule.type,
+        userId: vehicule.userId,
+        location: vehicule.location
+    })
+    
+    res.status(201).send(vehicule)
 })
 
 export { router as createVehicleRouter }
