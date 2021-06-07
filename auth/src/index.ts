90577f3fd8f739ca8fbaf6164dcbd151e7a96edc
phoboPT <hugo.branco.c@gmail.com>
2021-06-07 21:45:33 +0100

    update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -13,13 +13,13 @@ const start = async () => {
 
     try {
 
-        await natsWrapper.connect('auth', 'asdasd', 'http://nats-srv:4222')
-        natsWrapper.client.on('close', () => {
-            console.log('NATS connection closed')
-            process.exit()
-        })
-        process.on('SIGINT', () => natsWrapper.client.close())
-        process.on('SIGTERM', () => natsWrapper.client.close())
+        // await natsWrapper.connect('auth', 'asdasd', 'http://nats-srv:4222')
+        // natsWrapper.client.on('close', () => {
+        //     console.log('NATS connection closed')
+        //     process.exit()
+        // })
+        // process.on('SIGINT', () => natsWrapper.client.close())
+        // process.on('SIGTERM', () => natsWrapper.client.close())
 
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
