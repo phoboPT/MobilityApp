c264419d7e6c54be01ad219d8693a2b2b7651481
phoboPT <hugo.branco.c@gmail.com>
2021-06-07 21:48:53 +0100

    update

--- routes/src/index.ts
+++ routes/src/index.ts
@@ -9,13 +9,13 @@ const start = async () => {
     }
 
     try {
-        await natsWrapper.connect("route", "asdasd", "http://nats-srv:4222")
-        natsWrapper.client.on('close', () => {
-            console.log("NATS connection closed")
-            process.exit()
-        })
-        process.on('SIGINT', () => natsWrapper.client.close())
-        process.on('SIGTERM', () => natsWrapper.client.close())
+        // await natsWrapper.connect("route", "asdasd", "http://nats-srv:4222")
+        // natsWrapper.client.on('close', () => {
+        //     console.log("NATS connection closed")
+        //     process.exit()
+        // })
+        // process.on('SIGINT', () => natsWrapper.client.close())
+        // process.on('SIGTERM', () => natsWrapper.client.close())
 
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
