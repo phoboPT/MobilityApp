9262e743fab0a5bcbe55b6f2a4b573b943ab7b2b
phoboPT <hugo.branco.c@gmail.com>
2021-06-09 17:26:27 +0100

    update routes

--- routes/src/routes/index.ts
+++ routes/src/routes/index.ts
@@ -1,9 +1,10 @@
+import { requiredAuth } from "@mobileorg/common-lib"
 import express, { Response, Request } from "express"
 import { Route } from "../models/route"
 
 const router = express.Router()
 
-router.get('/api/routes', async (req: Request, res: Response) => {
+router.get('/api/routes', requiredAuth, async (req: Request, res: Response) => {
     const route = await Route.find({})
     res.send(route)
 })
