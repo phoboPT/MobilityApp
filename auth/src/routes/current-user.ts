6fc83e0c1545b9f321bb0d81407371a05eda283f
Hugo Cardoso <35729394+phoboPT@users.noreply.github.com>
2021-06-28 22:26:55 +0100

    added search users by id

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { currentUser } from '@mobileorg/common-lib';
+import { User } from '../models/user';
 const router = express.Router();
 
 router.get('/api/users/currentUser', currentUser, (req, res) => {
@@ -7,5 +8,11 @@ router.get('/api/users/currentUser', currentUser, (req, res) => {
     res.send({ currentUser: req.currentUser || null })
 })
 
+router.get('/api/users/:id', async (req, res) => {
+    const{id}=req.params
+   
+    const user = await User.findById(id)
 
+    res.send(user)
+})
 export { router as currentUserRouter }
