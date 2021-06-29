3155763b5d9289c0da5bfe4a8826dc3343dda3f4
Hugo Cardoso <35729394+phoboPT@users.noreply.github.com>
2021-06-29 15:54:12 +0100

    default avatar photo

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -18,7 +18,7 @@ router.post('/api/users/signup', [
         throw new BadRequestError('Email in use', { from: 'Signup, email is already in use' })
     }
 
-    const user = User.build({ email, password, name,photoUrl })
+    const user = User.build({ email, password, name,photoUrl:photoUrl||'https://toppng.com/uploads/preview/app-icon-set-login-icon-comments-avatar-icon-11553436380yill0nchdm.png' })
     await user.save()
 
     //Generate and setting token
