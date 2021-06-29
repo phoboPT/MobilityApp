a092ab34f89ea342fd9ef1e691c1bca0ae5f802e
Hugo Cardoso <35729394+phoboPT@users.noreply.github.com>
2021-06-29 18:37:39 +0100

    update show my vehicles

--- vehicles/src/routes/show.ts
+++ vehicles/src/routes/show.ts
@@ -1,10 +1,21 @@
-import { NotFoundError } from "@mobileorg/common-lib"
+import { currentUser, NotFoundError } from "@mobileorg/common-lib"
 import express, { Request, Response } from "express"
 import { Vehicle } from "../models/vehicle"
 
 
 
 const router = express.Router()
+
+router.get('/api/vehicles/me', currentUser,async (req: Request, res: Response) => {
+
+    const vehicle = await Vehicle.find({userId:req.currentUser?.id})
+
+    if (!vehicle) {
+        throw new NotFoundError({ from: 'show vehicle ' })
+    }
+    res.send(vehicle)
+
+})
 
 router.get('/api/vehicles/:id', async (req: Request, res: Response) => {
 
