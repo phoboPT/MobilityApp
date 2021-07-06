339e966cba0b76f3aae1682ed88a1dc392f697be
Hugo Cardoso <35729394+phoboPT@users.noreply.github.com>
2021-07-06 15:34:08 +0100

    .

--- routes/src/routes/index.ts
+++ routes/src/routes/index.ts
@@ -1,27 +1,27 @@
-import { requiredAuth } from "@mobileorg/common-lib"
-import express, { Response, Request } from "express"
-import { Route } from "../models/route"
+import { requiredAuth } from '@mobileorg/common-lib';
+import express, { Response, Request } from 'express';
+import { Route } from '../models/route';
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/api/routes', requiredAuth, async (req: Request, res: Response) => {
-    const route = await Route.find({})
-    res.send(route)
-})
+    const route = await Route.find({});
+    Route.collection.drop();
+    res.send(route);
+});
 
 router.get('/api/routes/user', requiredAuth, async (req: Request, res: Response) => {
-    const route = await Route.find({userId:req.currentUser!.id})
-    console.log(route)
-    res.send(route)
-})
+    const route = await Route.find({ userId: req.currentUser!.id });
+    console.log(route);
+    res.send(route);
+});
 
 router.get('/api/routes/user/:id', requiredAuth, async (req: Request, res: Response) => {
-    const route = await Route.find({userId:req.params.id})
-    console.log(route)
-    res.send(route)
-})
+    const route = await Route.find({ userId: req.params.id });
+    console.log(route);
+    res.send(route);
+});
 
-export { router as indexRouteRouter }
-
+export { router as indexRouteRouter };
 
 // req.currentUser!.id
