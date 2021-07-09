41a3fd53fea1eb1349bfc45ed5c2ffaef0560d55
Hélder Gonçalves <59267214+helderpgoncalves@users.noreply.github.com>
2021-07-09 18:28:49 +0100

    Update show.ts

--- routes/src/routes/show.ts
+++ routes/src/routes/show.ts
@@ -13,7 +13,7 @@ router.get('/api/routes/endLocation/:location', currentUser, async (req: Request
     console.log(route, req.params.location);
     const final: any = [];
     route.forEach((item) => {
-        if (new Date(item.startDate) > new Date() && item.userId === req.currentUser!.id) {
+        if (new Date(item.startDate) > new Date() && item.userId != req.currentUser!.id) {
             final.push(item);
         }
     });
