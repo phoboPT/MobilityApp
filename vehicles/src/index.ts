aefe42504e24dde78b1d175206f2148803c91018
phoboPT <hugo.branco.c@gmail.com>
2022-03-07 22:42:45 +0000

    teste

--- vehicles/src/index.ts
+++ vehicles/src/index.ts
@@ -3,40 +3,40 @@ import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
-    if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY not defined');
-    }
-    if (!process.env.MONGO_URI) {
-        throw new Error('MONGO_URI not defined');
-    }
-    if (!process.env.NATS_CLIENT_ID) {
-        throw new Error('NATS_CLIENT_ID not defined');
-    }
-    if (!process.env.NATS_URL) {
-        throw new Error('NATS_URL not defined');
-    }
-    if (!process.env.NATS_CLUSTER_ID) {
-        throw new Error('NATS_CLUSTER_ID not defined');
-    }
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY not defined');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI not defined');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID not defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL not defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID not defined');
+  }
 
-    try {
-        console.log('orders', process.env.NATS_CLIENT_ID, process.env.NATS_URL);
-        await natsWrapper.connect('orders', process.env.NATS_CLIENT_ID, process.env.NATS_URL);
-        natsWrapper.client.on('close', () => {
-            console.log('NATS connection closed');
-            process.exit();
-        });
-        process.on('SIGINT', () => natsWrapper.client.close());
-        process.on('SIGTERM', () => natsWrapper.client.close());
-
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('Connected to mongo DB');
-    } catch (err) {
-        console.error(`Error on start ${err}`);
-    }
-    app.listen(3000, () => {
-        console.log('Listening port 3000!!!!!!!!');
+  try {
+    console.log('orders', process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+    await natsWrapper.connect('orders', process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed');
+      process.exit();
     });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to mongo DB');
+  } catch (err) {
+    console.error(`Error on start ${err}`);
+  }
+  app.listen(3000, () => {
+    console.log('Listening port 3000!!!!!!!!');
+  });
 };
 
 start();
