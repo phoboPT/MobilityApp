cd933b14deb4f0db7e82a0065a3d7fc4612f5955
phoboPT <hugo.branco.c@gmail.com>
2022-03-08 17:38:34 +0000

    update

--- vehicles/src/index.ts
+++ vehicles/src/index.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
 
-
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY not defined');
